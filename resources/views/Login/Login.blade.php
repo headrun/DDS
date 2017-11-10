dd0bb919a3d73293b35140baf73d02621250cb8c
siddique4 <abu@headrun.net>
2017-11-10 12:35:52 +0530

    Login page

--- resources/views/Login/Login.blade.php
+++ resources/views/Login/Login.blade.php
@@ -5,21 +5,28 @@
 @stop
 @section('BaseCSSLib')
 <link rel="stylesheet" href="{{url()}}/assets/vendor/css/login.css">
-<!-- <style type="text/css">
-    .master-layout-class {
-        display: none;
+<style type="text/css">
+    body{
+        background-image: url({{url()}}/assets/vendor/img/bg.jpg);
     }
-</style> -->
+</style>
 @stop
 @section('BaseContent')
 <div class="container-fluid">
 
   <div class="row">
-      <div class="col-lg-6 col-md-6 visible-md visible-lg">
-        <img class="lhs-img" src="{{url()}}/assets/vendor/img/lhs.png"> 
+      <div class="col-lg-5 col-md-5 visible-md visible-lg">
+        <img class="lhs-img" src="{{url()}}/assets/vendor/img/green_back_for_white.png"> 
+        <img class="lhs-img circl" src="{{url()}}/assets/vendor/img/white_circle.png"> 
+        <img class="lhs-img logo" src="{{url()}}/assets/vendor/img/dds_logo.jpg"> 
+        <div class="lhs-img text">
+            
+              The D Cube Analytics Platform is built grounds up leveraging key business process knowledge at various functional levels while providing accelerator capabilities to disrupt the cost-time framework from an implementation standpoint
+            
+        </div>
       </div>
 
-      <div class="col-lg-6 col-sm-12 col-md-6 col-xs-12">
+      <div class="col-lg-7 col-sm-12 col-md-7 col-xs-12">
           <div class="row">
             <img class="logo-img pull-right" src="{{url()}}/assets/vendor/img/dcube_new.png">
           </div>
@@ -27,7 +34,7 @@
                 <div class="col-lg-2 col-md-2 visible-lg visible-md">
             
                 </div>
-                <div class="col-lg-8 col-sm-12 col-md-8 col-xs-12 shadow">
+                <div class="col-lg-7 col-sm-12 col-md-7 col-xs-12 shadow pad">
                   <form action="{{url()}}/login1" method="post">
                     {!! csrf_field() !!}
 
@@ -51,12 +58,20 @@
                 <div class="col-lg-2 col-md-2 visible-md visible-lg"></div>
           </div>
             <br>
-            <div>
-            <footer class="footer"><small class="copyright">&copy; <?php echo date("Y"); ?> Dcube. All Rights Reserved</small></footer></div>
+            
       </div>  
   </div>      
 </div>
+
+<div class="row">
+  <div class="col-md-offset-6 col-md-4 footer">
+
+      <small class="copyright">&copy; <?php echo date("Y"); ?> Dcube. All Rights Reserved</small>
+    
+  </div>
+</div>
 @stop
+
 @section('BaseJSLib')
 <script src="{{url()}}/assets/vendor/js/jquery.js"></script>
 @stop
